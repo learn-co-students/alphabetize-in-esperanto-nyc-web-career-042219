b38eaee04aceb1df154226bc17b65d973f04ba95
Adam Sultanov <adam.sultanov@gmail.com>
2019-04-13 14:20:04 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+# arr = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |phrase|
+    phrase.split("").map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end
+  end
 end
